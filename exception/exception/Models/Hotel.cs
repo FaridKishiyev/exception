e15a97ef431480541d1ca3314ec9d0694c82cb49
FaridKishiyev <99797424+FaridKishiyev@users.noreply.github.com>
2022-03-25 21:59:54 +0400

    3/25

---
+++ exception/exception/Models/Hotel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exception.Models
+{
+    class Hotel
+    {
+        public string Name;
+        private Room[] _Rooms;
+
+        public Hotel(string Name)
+        {
+            this.Name = Name;
+        }
+        public void AddRoom(Room[] x)
+        {
+             _Rooms = x;
+        }
+
+    }
+}
---
+++ exception/exception/Models/Room.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exception.Models
+{
+    class Room
+    {
+        private static int _id;
+        public string Name;
+        public double Price;
+        public int PersonCapacity;
+        public bool IsAvailable;
+        public static int _Id { get; }
+
+
+        public Room(string Name,double Price,int PersonCapacity, bool IsAvailable=true)
+        {
+            this.Name = Name;
+            this.Price = Price;
+            this.PersonCapacity = PersonCapacity;
+            this.IsAvailable = IsAvailable;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nPrice: {Price}\nPersonCapacity: {PersonCapacity}\nReservation: {IsAvailable}");
+        }
+    }
+}
